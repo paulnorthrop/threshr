f12d18e110467b7ca825b002fbabca0fe2d3ba16
Paul Northrop <p.northrop@ucl.ac.uk>
2016-11-24 23:32:10 +0000

    Initial commit of package: no code yet.

---
+++ R/threshr.R
@@ -0,0 +1,38 @@
+#' threshr: Threshold Selection and Uncertainty for Extreme Value Analysis
+#'
+#' Provides functions for the selection of extreme value threshold.
+#' See the 'threshr' website for more information, documentation and examples.
+#'
+#' @details The main function in the threshr package is
+#'
+#'   See \code{vignette("threshr-vignette", package = "threshr")} for an
+#'   overview of the package.
+#'
+#' @references Northrop, P. J. (2016). revdbayes: Ratio-of-Uniforms Sampling
+#'   for Bayesian Extreme Value Analysis. R package version 1.0.0.
+#'   \url{https://cran.r-project.org/package=revdbayes}.
+#'
+#' @seealso \code{\link[revdbayes]{set_prior}} and
+#'   \code{\link[revdbayes]{rpost}}: functions from the
+#'   \code{\link{revdbayes}} package used to set an extreme value prior density
+#'   and to sample from a extreme value posterior density respectively.
+#' @docType package
+#' @name threshr
+#' @import methods
+#' @importFrom stats runif
+NULL
+
+#' Storm peak significant wave heights from the Gulf of Mexico
+#'
+#' A numeric vector containing 315 hindcasts of storm peak significant wave
+#' heights, metres, from 1900 to 2005 at an unnamed location in the Gulf
+#' of Mexico.
+#'
+#'@format A vector containing 315 observations.
+#'@source Oceanweather Inc. (2005) GOMOS -- Gulf of Mexico hindcast study.
+#'@references Northrop, P. J., N. Attalides, and P. Jonathan. (2016).
+#'  Cross-Validatory Extreme Value Threshold Selection and Uncertainty with
+#'  Application to Ocean Storm Severity. \emph{Journal of the Royal
+#'  Statistical Society: Series C (Applied Statistics)}.
+#'  doi:\href{https://doi.org/10.1111/rssc.12159}{10.1111/rssc.12159}.
+"gom"
