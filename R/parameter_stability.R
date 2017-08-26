4a9e8a01e81c9daa1dddbf3bc6e393ee5ecf9ad1
Paul Northrop <p.northrop@ucl.ac.uk>
2017-08-26 01:36:16 +0100

    Minor cedit from u_vec to u_vec_gom. [ci skip]

--- R/parameter_stability.R
+++ R/parameter_stability.R
@@ -69,15 +69,15 @@
 #' @seealso \code{\link[stats]{quantile}}.
 #' @examples
 #' # Set a vector of thresholds
-#' u_vec <- quantile(gom, probs = seq(0, 0.95, by = 0.05))
+#' u_vec_gom <- quantile(gom, probs = seq(0, 0.95, by = 0.05))
 #'
 #' # Symmetric confidence intervals
-#' gom_stab <- stability(data = gom, u_vec = u_vec)
+#' gom_stab <- stability(data = gom, u_vec = u_vec_gom)
 #' plot(gom_stab)
 #'
 #' \dontrun{
 #' # Profile-likelihood-based confidence intervals
-#' gom_stab <- stability(data = gom, u_vec = u_vec, prof = TRUE)
+#' gom_stab <- stability(data = gom, u_vec = u_vec_gom, prof = TRUE)
 #' plot(gom_stab)
 #' }
 #' @export
