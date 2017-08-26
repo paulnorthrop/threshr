46b00955c188de3b9f21e474b5c097c804285d39
Paul Northrop <p.northrop@ucl.ac.uk>
2017-08-26 01:35:11 +0100

    Minor change from u_vec to u_vec_gom. [ci skip]

--- R/plot_and_summary.R
+++ R/plot_and_summary.R
@@ -70,8 +70,8 @@
 #' # [Smoother plots result from making n larger than the default n = 1000.]
 #'
 #' # Threshold diagnostic plot
-#' u_vec <- quantile(gom, probs = seq(0, 0.95, by = 0.05))
-#' gom_cv <- ithresh(data = gom, u_vec = u_vec, n_v = 4)
+#' u_vec_gom <- quantile(gom, probs = seq(0, 0.95, by = 0.05))
+#' gom_cv <- ithresh(data = gom, u_vec = u_vec_gom, n_v = 4)
 #' plot(gom_cv, lwd = 2, add_legend = TRUE, legend_pos = "topleft")
 #' mtext("significant wave height / m", side = 3, line = 2.5)
 #'
@@ -82,7 +82,7 @@
 #' summary(gom_cv)
 #'
 #' # Plot of Generalized Pareto posterior sample at the highest threshold
-#' n_u <- length(u_vec)
+#' n_u <- length(u_vec_gom)
 #' plot(gom_cv, which_u = n_u, points_par = list(pch = 20, col = "grey"))
 #' }
 #' @seealso \code{\link{ithresh}} for threshold selection in the i.i.d. case
@@ -265,8 +265,8 @@ plot.ithresh <- function(x, y, ..., which_v = NULL, prob = TRUE,
 #'   and/or \code{\link[graphics]{segments}}.
 #' @seealso \code{\link{stability}}.
 #' @examples
-#' u_vec <- quantile(gom, probs = seq(0, 0.95, by = 0.05))
-#' gom_stab <- stability(data = gom, u_vec = u_vec)
+#' u_vec_gom <- quantile(gom, probs = seq(0, 0.95, by = 0.05))
+#' gom_stab <- stability(data = gom, u_vec = u_vec_gom)
 #' plot(gom_stab)
 #' @export
 plot.stability <- function(x, y, ..., prob = TRUE,
@@ -392,8 +392,8 @@ plot.stability <- function(x, y, ..., prob = TRUE,
 #' }
 #' @examples
 #' \dontrun{
-#' u_vec <- quantile(gom, probs = seq(0, 0.95, by = 0.05))
-#' gom_cv <- ithresh(data = gom, u_vec = u_vec, n_v = 4)
+#' u_vec_gom <- quantile(gom, probs = seq(0, 0.95, by = 0.05))
+#' gom_cv <- ithresh(data = gom, u_vec = u_vec_gom, n_v = 4)
 #' summary(gom_cv)
 #' }
 #' @seealso \code{\link{ithresh}} for threshold selection in the i.i.d. case
@@ -448,8 +448,8 @@ summary.ithresh <- function(object, ...) {
 #'
 #' @examples
 #' \dontrun{
-#' u_vec <- quantile(gom, probs = seq(0, 0.95, by = 0.05))
-#' gom_cv <- ithresh(data = gom, u_vec = u_vec, n_v = 4)
+#' u_vec_gom <- quantile(gom, probs = seq(0, 0.95, by = 0.05))
+#' gom_cv <- ithresh(data = gom, u_vec = u_vec_gom, n_v = 4)
 #'
 #' # Note: gom_cv$npy contains the correct value of npy (it was set in the
 #' #       call to ithresh, via attr(gom, "npy").
