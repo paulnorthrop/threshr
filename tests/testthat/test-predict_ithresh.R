3dfbb8671ef29bfbd906d94625d8cf699918d107
Paul Northrop <p.northrop@ucl.ac.uk>
2017-08-30 00:46:10 +0100

    Add another test.

--- tests/testthat/test-predict_ithresh.R
+++ tests/testthat/test-predict_ithresh.R
@@ -5,10 +5,10 @@ context("predict.ithresh function")
 # Set a tolerance for the comparison of the simulated values
 my_tol <- 1e-5
 
-# Check that calling with type = "q" (quantiles) with the default
-# probabilities p = c(0.025, 0.25, 0.5, 0.75, 0.975) and then calling
-# with the results and with type = "p" gets us back to the initial
-# probabilities.
+# 1. Check that calling with type = "q" (quantiles) with the default
+#    probabilities p = c(0.025, 0.25, 0.5, 0.75, 0.975) and then calling
+#    with the results and with type = "p" gets us back to the initial
+#    probabilities.
 
 u_vec_gom <- quantile(gom, probs = seq(0, 0.9, by = 0.1))
 gom_cv <- ithresh(data = gom, u_vec = u_vec_gom)
@@ -19,3 +19,19 @@ ps <- predict(gom_cv, type = "p", x = qs, n_years = c(100, 1000))$y
 check_ps <- matrix(c(0.025, 0.25, 0.5, 0.75, 0.975), 5, 2)
 
 testthat::expect_equal(ps, check_ps, tolerance = my_tol)
+
+# 2. Check that the threshold-averaged distribution function lies in the
+#    envelope of the individual thresholds.
+
+# (Default: which_u = "all")
+ps <- predict(gom_cv, type = "p", which_u = "all")$y
+ave_ps <- ps[, 1]
+check_upper <- apply(ps[, -1], 1, max)
+check_lower <- apply(ps[, -1], 1, min)
+
+for (j in 1:length(check_upper)) {
+  testthat::expect_lt(ave_ps[j], check_upper[j])
+}
+for (j in 1:length(check_lower)) {
+  testthat::expect_gt(ave_ps[j], check_lower[j])
+}
