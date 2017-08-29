afbf0ea93032336bcdf88170cb007cd8372955a2
Paul Northrop <p.northrop@ucl.ac.uk>
2017-08-29 19:06:12 +0100

    Add codecov.

---
+++ tests/testthat/test_ithresh.R
@@ -0,0 +1,21 @@
+context("ithresh")
+
+# We check that the results from ithresh produced using use_rcpp = TRUE and
+# use_rcpp = FALSE are identical.
+
+# Set a tolerance for the comparison of the simulated values
+
+my_tol <- 1e-5
+n <- 1000
+seed <- 27082017
+
+u_vec_gom <- quantile(gom, probs = seq(0.05, 0.95, by = 0.45))
+# use_rcpp = TRUE
+set.seed(seed)
+res1 <- ithresh(data = gom, u_vec = u_vec_gom, n_v = 2)
+# use_rcpp = TRUE
+set.seed(seed)
+res2 <- ithresh(data = gom, u_vec = u_vec_gom, n_v = 2, use_rcpp = FALSE)
+
+testthat::expect_equal(res1$pred_perf, res2$pred_perf, tolerance = my_tol)
+testthat::expect_equal(res1$sim_vals, res2$sim_vals, tolerance = my_tol)
