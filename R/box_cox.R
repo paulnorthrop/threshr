3581439c1840d6c175f30651666cd5a46651c271
Paul Northrop <p.northrop@ucl.ac.uk>
2019-08-25 23:44:14 +0100

    Change default value of lambda_tol and add the order of the TS approximation as an argument

--- R/box_cox.R
+++ R/box_cox.R
@@ -1,7 +1,6 @@
 # =========================== box_cox_gm ===========================
 
-bc_gm <- function(x, lambda = 1, lngm = 0,
-                  lambda_tol = .Machine$double.eps) {
+bc_gm <- function(x, lambda = 1, lngm = 0, lambda_tol = 1 / 50, m = 4) {
   #
   # Computes the Box-Cox transformation of a vector.  If lambda is very close
   # to zero then a first order Taylor series approximation is used.
@@ -14,6 +13,7 @@ bc_gm <- function(x, lambda = 1, lngm = 0,
   #                original data vector.
   #   lambda_tol : A numeric scalar.  For abs(lambda) < lambda.tol use
   #                a Taylor series expansion.
+  #   m          : order of TS expansion (1 for linear, 1 for quadratic etc)
   # Returns:
   #   A numeric vector.  The transformed value
   #     (x^lambda - 1) / (lambda * gm ^ (lambda - 1),
@@ -29,14 +29,15 @@ bc_gm <- function(x, lambda = 1, lngm = 0,
   }
   max_len <- max(length(x), length(lambda))
   x <- rep_len(x, max_len)
-  lambda <- rep_len(lambda, max_len)
-  retval <- ifelse(abs(lambda) > lambda_tol, (x ^ lambda - 1) / lambda,
-                   ifelse(lambda == 0, log(x),
-                          ifelse(is.infinite(x),
-                                 ifelse(lambda < 0, -1 / lambda, Inf),
-                                 ifelse(x == 0, ifelse(lambda > 0, -1 / lambda,
-                                                       -Inf),
-                                        log(x) * (1 + lambda / 2)))))
+  la <- rep_len(lambda, max_len)
+  lnx <- log(x)
+  i <- 0:m
+  retval <- ifelse(abs(la) > lambda_tol, (x ^ la - 1) / la,
+              ifelse(la == 0, lnx,
+                ifelse(is.infinite(x),
+                  ifelse(la < 0, -1 / la, Inf),
+                    ifelse(x == 0, ifelse(la > 0, -1 / la, -Inf),
+                      sum(lnx ^ (i + 1) * la ^ i / factorial(i + 1))))))
   retval <- retval * exp((1 - lambda) * lngm)
   return(retval)
 }
